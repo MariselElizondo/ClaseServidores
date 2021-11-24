4e2730da01b809517caba90164fe767756304d7a
Casandra Marisel Elizondo <elizondo.celular@gmail.com>
2021-11-24 13:17:01 -0300

    Init products

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
 const express = require('express')
 const app = express()
-app.listen(process.env.PORT || 8080)
 
-let products = [
-    {
-      "title": "Escuadra",
-      "price": 123.45,
-      "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
-      "id": 1
-    },
-    {
-      "title": "Calculadora",
-      "price": 234.56,
-      "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png",
-      "id": 2
-    },
-    {
-      "title": "Globo Terráqueo",
-      "price": 345.67,
-      "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png",
-      "id": 3
-    }
-]
+//EJECUCIÓN
+const Contenedor = require("./Contenedor.js")
+let containerOne= new Contenedor('productos.txt')
+
+const init = async () => {
+    console.log('================= AGREGO ELEMENTOS INICIALES =================')
+    await containerOne.save({"title":"Escuadra","price":123.45,"thumbnail":"https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png"})
+    await containerOne.save({"title":"Calculadora","price":234.56,"thumbnail":"https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png"})
+    await containerOne.save({"title":"Globo Terráqueo","price":345.67,"thumbnail":"https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png"})
+}
+
+init()
+
+app.listen(process.env.PORT || 8080)
 
 //RUTAS
 app.get('/', (req,res) => {
